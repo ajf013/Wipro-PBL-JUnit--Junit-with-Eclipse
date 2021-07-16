ef5fb2b622bd31f6fa5d636fbd4a794fa57ee1ec
Francis Cruz <jeni13franc@gmail.com>
2021-07-16 10:12:58 +0530

    Initial commit

---
+++ MyUnit.java
@@ -0,0 +1,9 @@
+package assignment1;
+
+public class MyUnit {
+	public String stringConcat(String a, String b) {
+		return a + b;
+	}
+
+}
+
---
+++ MyUnitTest.java
@@ -0,0 +1,15 @@
+package assignment1;
+
+class MyUnitTest {
+
+	void testStringConcat() {
+		MyUnit myUnit = new MyUnit();
+		assertEquals("Result", "HelloWorld", myUnit.stringConcat("Hello", "World"));
+	}
+
+	private void assertEquals(String string, String string2, String stringConcat) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
